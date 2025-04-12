6e02b821a9ea8ac30a2650b9058915c91cbbf0a3
KlowdyTMS <victor.souza2210@gmail.com>
2025-04-12 10:06:12 -0300

    feat: sound effect to add items

--- src/app/components/keyboard-input/keyboard-input.component.ts
+++ src/app/components/keyboard-input/keyboard-input.component.ts
@@ -11,19 +11,28 @@ import { FormsModule } from '@angular/forms';
 export class KeyboardInputComponent {
   inputValue: string = '';
 
-  constructor(private cdr: ChangeDetectorRef) { }
+  constructor(private cdr: ChangeDetectorRef) {}
 
   @HostListener('document:keydown', ['$event'])
   handleKeyPress(event: KeyboardEvent): void {
     if (event.key === 'Enter') {
       event.preventDefault();
       this.inputValue += '\n';
+      this.playSound();
     } else if (event.key === 'Backspace') {
       event.preventDefault();
       this.inputValue = this.inputValue.slice(0, -1);
       this.cdr.detectChanges();
     } else if (event.key.length === 1 && !event.ctrlKey && !event.metaKey) {
       this.inputValue += event.key;
+      this.playSound();
     }
+  }
+
+  private playSound(): void {
+    const audio = new Audio(
+      'https://github.com/Do-nada-ao-tudo/RepoStaticFile/raw/9ca9c2bc35872f988a2e1daaa7f4087bd1c5794b/sound-bip-alert.mp3'
+    );
+    audio.play();
   }
 }
